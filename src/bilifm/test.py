a954659ea9f847a3105e914465e2954cb90ab7f4
抽象 <1930278836@qq.com>
2024-10-03 14:13:46 +0800

    Fix github action envs,update unittest

--- src/bilifm/test.py
+++ src/bilifm/test.py
@@ -1,8 +1,6 @@
 import os
 import unittest
 
-from typing_extensions import Annotated
-
 from .command import *
 from .util import AudioQualityEnums, change_directory
 
@@ -13,8 +11,7 @@ class TestBiliFM(unittest.TestCase):
         cls.cpwd = os.getcwd()
         change_directory("tests")
 
-    @classmethod
-    # 清理测试文件
+    @classmethod  # 清理测试文件
     def tearDownClass(cls) -> None:
         change_directory(cls.cpwd)
         import shutil
@@ -35,27 +32,31 @@ class TestBiliFM(unittest.TestCase):
         self.test_fav = "69361944"  # 暂时没有找到比较好的测试fav
         self.test_cookies_path = ""
         self.media_id = "69361944"
-        self.sid = "1855309"  # 需要找到一个较少的测试合集，不然测起来时间太久了
-        self.uid = "261485584"  # 需要找到一个作品少的uid进行测试
+        self.test_series_uid = "25876945"
+        self.test_series_sid = "273179"
+        self.uid = "16"
+        self.test_season_uid = "48484716"
+        self.test_season_sid = "28722"
 
     def test_bv_command(self):
         self.assertIsNone(bv(self.test_bv, None, self.audioquality))
 
     def test_uid_command(self):
-        # self.assertIsNone(uid(self.uid, None, self.audioquality))
-        pass
+        self.assertIsNone(uid(self.uid, None, self.audioquality))
 
     def test_fav_command(self):
         # 需要cookies 感觉不太能测吧
         pass
 
     def test_season_command(self):
-        # self.assertIsNone(season(self.uid, self.sid, None, self.audioquality))
-        pass
+        self.assertIsNone(
+            season(self.test_season_uid, self.test_season_sid, None, self.audioquality)
+        )
 
     def test_series_command(self):
-        # self.assertIsNone(series(self.uid, self.sid, None, self.audioquality))
-        pass
+        self.assertIsNone(
+            series(self.test_series_uid, self.test_series_sid, None, self.audioquality)
+        )
 
 
 if __name__ == "__main__":
